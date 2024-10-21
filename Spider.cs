d5fc1265c698d484c0c813bd7146b5952522131a
Enator18 <62544405+Enator18@users.noreply.github.com>
2024-10-20 23:06:46 -0400

    added camera rotation

--- Spider.cs
+++ Spider.cs
@@ -5,6 +5,15 @@ public partial class Spider : CharacterBody3D
 {
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
+	public const float LookSpeed = 0.005f;
+
+	private SpringArm3D cameraArm;
+
+	public override void _Ready()
+	{
+		Input.SetMouseMode(Input.MouseModeEnum.Captured);
+		cameraArm = GetNode<SpringArm3D>("CameraArm");
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -16,15 +25,20 @@ public partial class Spider : CharacterBody3D
 			velocity += GetGravity() * (float)delta;
 		}
 
+		if (Input.IsActionPressed("Quit"))
+		{
+			GetTree().Quit();
+		}
+
 		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		if (Input.IsActionJustPressed("Jump") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
 		{
@@ -39,5 +53,17 @@ public partial class Spider : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+	
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if(@event is InputEventMouseMotion mouseEvent)
+		{
+			float xRotation = Rotation.Y - (mouseEvent.Relative.X * LookSpeed);
+			float yRotation = cameraArm.Rotation.X + (mouseEvent.Relative.Y * LookSpeed);
+			yRotation = Math.Max(Math.Min(yRotation, 10.0f * (float)Math.PI / 180.0f), -90.0f * (float)Math.PI / 180.0f);
+			SetRotation(new Vector3(Rotation.X, xRotation, Rotation.Z));
+			cameraArm.SetRotation(new Vector3(yRotation, cameraArm.Rotation.Y, cameraArm.Rotation.Z));
+		}
 	}
 }
